057300e998d8eb3c0c93f7dec75e4e0ba3791edc
Dorblx <105562606+lolthatseazy@users.noreply.github.com>
2025-11-15 06:39:18 -0300

    Add new member 'Horizon' to Members.lua

--- Members.lua
+++ Members.lua
@@ -2,4 +2,5 @@
     "751f1564afbc009a8e593383a2d902ed39c47d0a", -- Dorblx
     "afce53debaa995b8b182b206733a7a23a4a51ae5", -- Rethix
     "43afbb63f3a107c788d03e6f2189d537cfc0b603", -- Mateymate
+    "???", -- Horizon
 }

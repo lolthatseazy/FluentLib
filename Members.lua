4ceff3316891a586f534098c277c4375d7c95302
Dorblx <105562606+lolthatseazy@users.noreply.github.com>
2025-11-27 03:28:17 -0300

    Automated Update - ERX Bot

--- Members.lua
+++ Members.lua
@@ -1,6 +1,6 @@
 {
     "778273e2ef6ab5a387e09211c3b5a1fb496b946f", -- Dorblx
-    "afce53debaa995b8b182b206733a7a23a4a51ae5", -- Rethix
+    "21ac6e3da8cd82340de45ba7c27c32e524e408aa", -- Rethix
     "ac4a0a603ea034ffd69b6b868a4b40eb5f378feb", -- Mateymate
     "efe22fd2c65f115f5ca8abe0c81790e301f6cef8", -- Horizon
 }

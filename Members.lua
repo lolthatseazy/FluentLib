b2902e0a48014bb78790105f4e7189b78e43e39d
Dorblx <105562606+lolthatseazy@users.noreply.github.com>
2025-12-27 15:27:04 -0300

    Automated Update - ERX Bot

--- Members.lua
+++ Members.lua
@@ -4,6 +4,6 @@
     "54fce466b352a0d0c2cd8fbf2a165525fb043610", -- Mateymate
     "efe22fd2c65f115f5ca8abe0c81790e301f6cef8", -- Horizon
     "???", -- Merf
-    "bbe51cd53c36a0feb80115d2c80f5bddac354516", -- Sander
+    "0738e8cbbae5e843414a88c04c98d6e94209c55a", -- Sander
     "57eab662c07ea00bf48417d57e0d4f942802e701", -- Silly
 }

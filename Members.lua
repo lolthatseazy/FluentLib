d4edcdddde8f35fa7cb83fe8660b8e3e34b20fdd
Dorblx <105562606+lolthatseazy@users.noreply.github.com>
2026-01-01 12:38:30 -0300

    Automated Update - ERX Bot

--- Members.lua
+++ Members.lua
@@ -4,5 +4,5 @@
     "f389c5bc47752b4979488d99d649fa72f96bf354", -- Mateymate
     "f3cf6c0895d16aac688a82dcbbc48f95b9ebec02", -- Sander
     "65f5e4d6554c1f9e6a7aa14e825274441cc4d54f", -- 3than
-    "274dcd0f1988ab5ee836c539e660d2abc13b9d2b", -- LCSO
+    "bdc3fda363b8a7d15ee1fbc9016a49b00cb8d786", -- LCSO
 }

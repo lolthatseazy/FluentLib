df65ad4299d49da5ca3f6ea8c981ed93c4592fda
Dorblx <105562606+lolthatseazy@users.noreply.github.com>
2025-12-31 23:42:00 -0300

    Update Members.lua

--- Members.lua
+++ Members.lua
@@ -2,9 +2,6 @@
     "a6a4b3a4414c9e021e657dc8df6752c22663de2e", -- Dorblx
     "e0770fcb8edcdf0f6c168769194e7ce873e3f6b1", -- Rethix
     "778273e2ef6ab5a387e09211c3b5a1fb496b946f", -- Mateymate
-    "efe22fd2c65f115f5ca8abe0c81790e301f6cef8", -- Horizon
-    "???", -- Merf
     "ae1e3272d197fd119b334fd25123090dcdb0cf84", -- Sander
-    "57eab662c07ea00bf48417d57e0d4f942802e701", -- Silly
     "65f5e4d6554c1f9e6a7aa14e825274441cc4d54f", -- 3than
 }
